915ad396cf25858610de2f5f6dbecfbb60a334a5
james rochabrun <jamesrochabrun@gmail.com>
2020-05-22 12:44:06 -0700

    color literalse

--- SwiftUIIntro/MenuView.swift
+++ SwiftUIIntro/MenuView.swift
@@ -12,6 +12,7 @@ import SwiftUI
  
  - start from the content first not the container
  - for more info about fonts https://designcode.io/swiftui-sf-symbols-and-props minute 3:40
+  - Color.white also works as a view!
  */
 
 struct MenuView: View {
@@ -19,16 +20,37 @@ struct MenuView: View {
         VStack {
             Spacer()
             VStack(spacing: 16) {
+                Text("Meng - 28% complete")
+                    .font(.caption)
+                
+                Color.white
+                    .frame(width: 38, height: 6)
+                    .cornerRadius(3)
+                    .frame(width: 130, height: 6, alignment: .leading)
+                    .background(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.08))
+                    .cornerRadius(3)
+                    .padding() // defaults to 16 all edges.
+                    .background(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.1))
+                    .cornerRadius(12)
+                
                 MenuRow(title: "Account", icon: "gear")
                 MenuRow(title: "Billing", icon: "creditcard")
                 MenuRow(title: "Sign out", icon: "person.crop.circle")
             }
             .frame(maxWidth: .infinity)
             .frame(height: 300)
-            .background(Color.white)
+            .background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), Color(#colorLiteral(red: 0.8705882353, green: 0.8941176471, blue: 0.9450980392, alpha: 1))]), startPoint: .top, endPoint: .bottom))
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .shadow(radius: 30)
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
             .padding(.horizontal, 30)
+            .overlay(
+                Image("Avatar")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 60, height: 60)
+                    .clipShape(Circle())
+                    .offset(y: -150)
+            )
         }
         .padding(.bottom, 30)
     }
@@ -52,6 +74,7 @@ struct MenuRow: View {
                 .font(.system(size: 20, weight: .bold))
                 .imageScale(.large)
                 .frame(width: 32, height: 32)
+                .foregroundColor(Color(#colorLiteral(red: 0.662745098, green: 0.7333333333, blue: 0.831372549, alpha: 1)))
             Text(title)
                 .font(.system(size: 20, weight: .bold, design: .rounded))
                 .frame(width: 120, alignment: .leading)
