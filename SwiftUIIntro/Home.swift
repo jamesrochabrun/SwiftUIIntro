c1bf65ffad668b53d8f609d3420a7ccdf140ad96
james rochabrun <jamesrochabrun@gmail.com>
2020-05-22 14:58:53 -0700

    data progress and tap background

--- SwiftUIIntro/Home.swift
+++ SwiftUIIntro/Home.swift
@@ -14,11 +14,15 @@ import SwiftUI
  - using  .edgesIgnoringSafeArea(.all) to the container will make every sub view ignore safe areas.
  - not to the container!
  - suggestion: apply offset before rotation modifyers
+ - .background(Color.black.opacity(0.001)) 0r .background(Color.clear) setting it to 0 will make it not interactive
+ - the animtion modifyiers does not go chained to the festure but to the list of modifyiers of the view.
+ - view state height can be used to apply in corner radius, opacity etc.
  */
 
 struct Home: View {
     
     @State var showProfile = false
+    @State var viewState: CGSize = .zero
     
     var body: some View {
         
@@ -52,14 +56,34 @@ struct Home: View {
                 .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
                 .shadow(color: Color.black.opacity(0.2), radius: 20, x: 20, y: 20)
                 .offset(y: showProfile ? -450 : 0)
-                .rotation3DEffect(Angle(degrees: showProfile ? -10 : 0), axis: (x: 10.0, y: 0, z: 0))
+                /// - 10 will add that depth effect in the #D
+                /// dividing by 10 the animation is less aggressive because is kind of doing the 10 % of the dragging translation value
+                .rotation3DEffect(Angle(degrees: showProfile ? Double(viewState.height / 10) - 10.0 : 0), axis: (x: 10.0, y: 0, z: 0))
                 .scaleEffect(showProfile ? 0.9 : 1)
                 .edgesIgnoringSafeArea(.all)
                 .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
             
             MenuView()
-                .offset(y: showProfile ? 0 : 600)
-                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+                .background(Color.black.opacity(0.001)) // kind of user interaction enabled?
+                
+                .offset(y: showProfile ? 0 : 1000)
+                .offset(y: viewState.height) // allows dragging listens to the drag change
+                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0)) // you add the animation chaining the modifyiers
+                .onTapGesture {
+                    self.showProfile.toggle()
+            }
+            .gesture(
+                DragGesture().onChanged { value in
+                    self.viewState = value.translation
+                }.onEnded { value in
+                    /// logic to dismiss card.
+                    if self.viewState.height > 50 {
+                        self.showProfile = false
+                    }
+                    self.viewState = .zero
+                    
+                }
+            )
         }
     }
 }
