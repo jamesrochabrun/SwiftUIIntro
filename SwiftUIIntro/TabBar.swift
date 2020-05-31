194968a492ebcde20151a719773a9b4e08a3dc5d
james rochabrun <jamesrochabrun@gmail.com>
2020-05-31 12:42:46 -0700

    tab bar items

---
+++ SwiftUIIntro/TabBar.swift
@@ -0,0 +1,47 @@
+//
+//  TabBar.swift
+//  SwiftUIIntro
+//
+//  Created by James Rochabrun on 5/31/20.
+//  Copyright © 2020 James Rochabrun. All rights reserved.
+//
+
+import SwiftUI
+
+/*
+ 
+ .edgaesIgnoringSafeArea is no longer needed after Xcode 11.4.1
+ **/
+
+struct TabBar: View {
+    var body: some View {
+        TabView {
+            Home().tabItem {
+                Image(systemName: "play.circle.fill")
+                Text("Home")
+            }
+            ContentView().tabItem {
+                Image(systemName: "rectangle.stack.fill")
+                Text("Certificates")
+            }
+        }
+        .edgesIgnoringSafeArea(.top)
+    }
+    
+//    func buildTabItem(_ title: String, image: String) -> TupleView<Text> {
+//        
+//    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        //// Previewing devices
+        /// Multiple devices does not wiork with live preview
+        /// use Group for multiple devices.
+        /// option click in `previewDevice` to see device syntaxes/
+        Group {
+            TabBar().previewDevice("iPhone 8")
+        }
+    }
+}
